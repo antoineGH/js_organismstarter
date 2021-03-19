bfb8780499caf8e95255a23f8eb1dbcb07e9f087
Templars <antoine.ratat@gmail.com>
2021-03-19 14:39:37 +0800

    mutate method

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
+	const dnaBases = ['A', 'T', 'C', 'G']
+	return dnaBases[Math.floor(Math.random() * 4)]
+}
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
-  for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
-  }
-  return newStrand;
-};
+	const newStrand = []
+	for (let i = 0; i < 15; i++) {
+		newStrand.push(returnRandBase())
+	}
+	return newStrand
+}
 
+// Returns a specimen with random DNA stand
+const pAequorFactory = (specimenNum, dna) => {
+	return {
+		specimenNum: specimenNum,
+		dna: dna,
+		mutate() {
+			console.log(`\n--- Mutate ---`)
+			console.log(`Before Mutation - ${dna}`)
+			const randomIdx = Math.floor(Math.random() * dna.length)
+			const toChange = dna[randomIdx]
+			let randomDNA = returnRandBase()
+			while (toChange === randomDNA) {
+				randomDNA = returnRandBase()
+			}
+			console.log(`Mutating from ${toChange} to ${randomDNA} at position ${randomIdx}`)
+			dna[randomIdx] = randomDNA
+			console.log(`After Mutation - ${dna}`)
+			console.log(`--- End Mutate ---\n`)
+		},
+	}
+}
 
-
-
-
-
-
-
+const spec1 = pAequorFactory(1, mockUpStrand())
+spec1.mutate()
